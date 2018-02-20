df940c6756641c47b89f78a3a4e8686326a624a5
Amit Marx <amitm@wix.com>
2018-02-20 19:30:41 +0200

    refactor a little again

--- the-wonderful-option-space.scala
+++ the-wonderful-option-space.scala
@@ -21,11 +21,11 @@ def plus(a: Double, b: Double): Option[Double] = {
 
 def Add100EachAndThenFoldDivide(str:String) = {
   val splitedValues = str.split(",")
-  val add100function = plus(_, 100)
+  val add100function = plus(_:Double, 100)
 
   val convertedValues = splitedValues.map(toDoubleOption)
   val valuesIncreasedBy100 = convertedValues.map(_.flatMap(add100function))
-  val foldDivideValue = valuesIncreasedBy100.fold(Some(1.0))((acc, value) => {
+  val foldDivideValue = valuesIncreasedBy100.reduce((acc, value) => {
     (acc, value) match {
       case (Some(a: Double), Some(b: Double)) => divide(a, b)
       case _ => None
@@ -33,6 +33,7 @@ def Add100EachAndThenFoldDivide(str:String) = {
   })
   foldDivideValue
 }
-Add100EachAndThenFoldDivide("100,50,1000,60,64.5") //Some(1.1513309385649811E-12)
-Add100EachAndThenFoldDivide("100,50,1000,BLA,60,64.5") //res1: Option[Double] = None
-Add100EachAndThenFoldDivide("100,50,-100") //res1: Option[Double] = None
+Add100EachAndThenFoldDivide("99900,100") //res0: Option[Double] = Some(500.0)
+Add100EachAndThenFoldDivide("99900,100,0") //res1: Option[Double] = Some(5.0)
+Add100EachAndThenFoldDivide("100,50,1000,BLA,60,64.5") //res2: Option[Double] = None
+Add100EachAndThenFoldDivide("100,50,-100") //res3: Option[Double] = None
